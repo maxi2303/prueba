e77d3a9bf63d7bb0263f1c47f30b9d00c03f2b8e
maxi2303 <31107964+maxi2303@users.noreply.github.com>
2017-10-19 18:01:34 -0300

    array list

---
+++ ejercicio1/Persona.java
@@ -0,0 +1,20 @@
+
+package ejercicio1;
+
+public class Persona {
+
+    public String nombre;
+       
+       public String apellido;
+       
+       public String legajo;
+       
+       public Persona()
+       
+        {
+            System.out.println("constructor"); 
+        }
+    
+
+
+}
