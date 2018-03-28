a680444792961e53ac4dfbfc53ccd33476221e86
maxi2303 <31107964+maxi2303@users.noreply.github.com>
2018-03-28 16:14:48 -0300

    clase foto

---
+++ visordeimagenesmdi/VisorDeImagenesMDI.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package visordeimagenesmdi;
+
+
+import Vistas.FrmInicioPrincipal;
+
+
+/**
+ *
+ * @author alumno
+ */
+public class VisorDeImagenesMDI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //System.out.println("inicio");
+        FrmInicioPrincipal frm= new FrmInicioPrincipal();
+        frm.setVisible(true);
+        
+    }
+    
+}
